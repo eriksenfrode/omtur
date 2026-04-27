3ee7a92b6d79a7e35dfdc5b147a745d6e82d9eb5
Frode Eriksen <eriksen.frode@proton.me>
2026-04-27 10:22:17 +0200

    Tillat Facebook crawler på annonsesider

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/annonser/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
