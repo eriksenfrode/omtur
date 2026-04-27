76d713e6a63c05d887ee5096580253fe7ce69c20
Frode Eriksen <eriksen.frode@proton.me>
2026-04-27 14:13:21 +0200

    Tøm redirects i next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,17 @@
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'query', key: 'fbclid' }],
-        destination: '/:path*',
-        permanent: false,
-      },
-    ]
-  },
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'all',
-          },
-        ],
-      },
-      {
-        source: '/api/og/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET',
-          },
+          { key: 'X-Robots-Tag', value: 'all' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
         ],
       },
     ]
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/og/:id',
-        destination: '/api/og/:id',
-      },
-    ]
+  async redirects() {
+    return []
   },
 }
 export default nextConfig
